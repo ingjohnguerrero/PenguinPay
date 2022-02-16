b6fba94b3227c8a33294ab76070b1c0c87290238
John Guerrero <ingjohnguerrero@gmail.com>
2022-02-16 10:16:52 -0500

    feat: Add exchanges rates mapping functionality

--- Penguin Pay/ModelLayer/MockedWebServer/MockedExchangeService.swift
+++ Penguin Pay/ModelLayer/MockedWebServer/MockedExchangeService.swift
@@ -12,6 +12,19 @@ class MockedExchangeService: ExchangeService {
     /// Get latest exchange rates from json files
     /// - Parameter completion: Response handling closure
     func latestRates(completion: @escaping ([Currency], Error?) -> Void) {
-        completion([], nil)
+        guard let jsonData = JsonHelper
+                .data(from: .exhangeRates) else { return completion([], ExchangeServiceError.unableToLoadMock) }
+        do {
+            let latestExchangesDTO = try JSONDecoder().decode(LatestExchangesDTO.self, from: jsonData)
+            var currencyRates: [Currency] = []
+            currencyRates.append(Currency(countryCode: "KES", rate: latestExchangesDTO.rates.kenya))
+            currencyRates.append(Currency(countryCode: "NGN", rate: latestExchangesDTO.rates.nigeria))
+            currencyRates.append(Currency(countryCode: "TZS", rate: latestExchangesDTO.rates.tanzania))
+            currencyRates.append(Currency(countryCode: "UGX", rate: latestExchangesDTO.rates.uganda))
+            completion(currencyRates, nil)
+        } catch {
+            print(String(describing: error))
+            completion([], error)
+        }
     }
 }
