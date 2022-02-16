b7da54fe42380b6412659542da37e59e2daffca2
John Guerrero <ingjohnguerrero@gmail.com>
2022-02-16 07:17:54 -0500

    Initial commit

---
+++ Penguin Pay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Penguin Pay
+//
+//  Created by John Edwin Guerrero Ayala on 16/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
